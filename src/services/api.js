267dd127e2506ebe9f0ec84b73c9720dacb69562
anna-zau <annazauholnikova555@gmail.com>
2022-12-13 13:36:44 +0200

    image finder ready

---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '30925450-e74dba54a3b0c628a6a6e8d3f';
+
+export const fetchImages = async (searchValue, page) => {
+  const response = await axios.get(
+    `?q=${searchValue}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  );
+
+  const images = response.data.hits.map(image => {
+    const { id, tags, webformatURL, largeImageURL } = image;
+    return {
+      id,
+      tags,
+      webformatURL,
+      largeImageURL,
+    };
+  });
+  //   console.log(images);
+  return images;
+};
